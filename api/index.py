1fc74a610b18d36ffc168858a1ebbe8aa06406aa
alloha-sy <dis.awezing80@gmail.com>
2023-12-17 02:17:15 +0800

    up4

--- api/index.py
+++ api/index.py
@@ -4,19 +4,19 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Use a service account.
-cred = credentials.Certificate('key.json')
-# r'C:\Users\User\Desktop\py\key.json'
-app = firebase_admin.initialize_app(cred)
+# cred = credentials.Certificate('key.json')
+# # r'C:\Users\User\Desktop\py\key.json'
+# app = firebase_admin.initialize_app(cred)
 
-db = firestore.client()
+# db = firestore.client()
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def home():
-    doc_ref = db.collection("posts").document("WasXXBdrVywtUc7pz8F6")
-    doc = doc_ref.get()
+    # doc_ref = db.collection("posts").document("WasXXBdrVywtUc7pz8F6")
+    # doc = doc_ref.get()
 
-    return jsonify({"data":doc.to_dict(),"id":doc.id})
+    return jsonify("hello")
 
 if __name__ == "__main__":
     app.run(debug=True)
